0900cc4e39d2d8c0debe2f27ac03d6ef2700712e
jpra0204 <jp.roca.angulo@gmail.com>
2025-05-19 14:49:06 -0400

    fixing all the Tailwind errors, consoloditating configs

---
+++ apps/app-web/pages/test-tailwind.tsx
@@ -0,0 +1,8 @@
+export default function TestTailwindPage() {
+    return (
+      <div className="bg-background text-primary font-poppins p-md rounded-md">
+        ✅ This uses utility classes, no inline styles!
+      </div>
+    );
+  }
+  
